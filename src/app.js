74ffb3deac9e80064128516d5231d8d6dd80b0ee
Sunny <dharmindersingh.gne@gmail.com>
2026-05-31 22:09:50 +0100

    security middlewares, routes tested

--- src/app.js
+++ src/app.js
@@ -1,9 +1,57 @@
+import logger from '#config/logger.js';
+import authRoutes from '#routes/auth.routes.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 
+// helmet for security headers.
+app.use(helmet());
+
+// express.json for parsing json bodies.
+app.use(express.json());
+
+// express.urlencoded for parsing urlencoded bodies.
+app.use(express.urlencoded({ extended: true }));
+
+// morgan for monitoring http requests and debug.
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
+// cors for cross-origin requests.
+app.use(cors());
+
+//cookie parser for parsing cookies.
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
+  logger.info('Hello acquisitions');
   res.status(200).send('Hello acquisitions');
+});
+
+// auth routes..
+app.use('/api/auth', authRoutes);
+
+// health check route.
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+// api routes..
+app.get('/api', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to the Acquisitions API',
+  });
 });
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import app from './app.js';
+import logger from '#config/logger.js';
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+  logger.info(`Listening on http://localhost:${PORT}`);
 });
